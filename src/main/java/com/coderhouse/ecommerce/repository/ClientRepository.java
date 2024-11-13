6bf5e878d1b2798ffa07a023616f32d15e0e4d9e
Valentin Galfre <galfre.vn@gmail.com>
2024-11-12 22:32:30 -0300

    Coderhouse :: Deprecating Client entity in favour of Customer

--- src/main/java/com/coderhouse/ecommerce/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coderhouse.ecommerce.repository;
-
-import com.coderhouse.ecommerce.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
