471626027bce2d3a0283e6dddde4f5d95b9d18ac
Valentin Galfre <galfre.vn@gmail.com>
2024-11-12 22:32:55 -0300

    Coderhouse :: Deprecating InvoiceDetails entity

--- src/main/java/com/coderhouse/ecommerce/repository/InvoiceDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coderhouse.ecommerce.repository;
-
-import com.coderhouse.ecommerce.model.InvoiceDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceDetailsRepository extends JpaRepository<InvoiceDetails, Integer> {
-}
